6af91df43073d7c957e6ea786f7675d91f3d7b2d
PeterSolution <piotrwolff@op.pl>
2024-03-18 14:53:24 +0100

    commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls.Maps;
+using Windows.Devices.Geolocation;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
+
+namespace map
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            map.Style = MapStyle.Terrain;
+            BasicGeoposition cityPosition = new BasicGeoposition()
+            {
+                Latitude = 54.35603333,
+                Longitude = 18.64612007
+            };
+            Geopoint cityCenter = new Geopoint(cityPosition);
+
+            map.Center = cityCenter;
+            map.ZoomLevel = 12;
+        }
+
+
+        public void powieksz()
+        {
+            map.ZoomLevel++;
+        }
+        public void oddal()
+        {
+            map.ZoomLevel--;
+        }
+        public void satelita()
+        {
+
+        }
+        public void koordynaty() 
+        { 
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            powieksz();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            oddal();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (map.Style == MapStyle.Terrain)
+            {
+                map.Style = MapStyle.Aerial;
+            }
+            else
+            {
+                map.Style = MapStyle.Terrain;
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(pagekoordynaty));
+            Frame.BackStack.Clear();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            Geopoint city = e.Parameter as Geopoint;
+            if (city != null)
+            {
+                map.Center = city;
+            }
+        }
+        
+    }
+
+    
+}
---
+++ pagekoordynaty.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Text;
+using System.Runtime.Serialization.Json;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace map
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class pagekoordynaty : Page
+    {
+        
+        string link = "http://dev.virtualearth.net/REST/v1/Locations?q=";
+        string city="";
+        string key = "&key=CgZKS8wrR5vblv8CmSMI~byS6UYfcdjbExSmJHDOtqA~AjNB7bfiB8k9tHLod_9Ua1-r_dV6gL17-htjQ0Y0z7WFYNhBa7OhlW-ZCVIwAOfP";
+        public pagekoordynaty()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btnszuk_Click(object sender, RoutedEventArgs e)
+        {
+            if (boxdestination.Text != null)
+            {
+                city=boxdestination.Text;
+                string json=link+city+key;
+                /*using(HttpClient client =new HttpClient())
+                {
+                    var file = await client.GetAsync(json);
+                    var datafromfile = await file.Content.ReadAsStringAsync();
+                    var filee=new DataContractJsonSerializer(typeof)
+                }*/
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetAsync(json);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+
+                        // Parsowanie JSON do obiektu JObject
+                        var data = JObject.Parse(content);
+
+                        // Pobranie danych z obiektu JObject
+                        var resourceSets = data["resourceSets"];
+                        if (resourceSets != null && resourceSets.HasValues)
+                        {
+                            foreach (var resourceSet in resourceSets)
+                            {
+                                var resources = resourceSet["resources"];
+                                if (resources != null && resources.HasValues)
+                                {
+                                    foreach (var resource in resources)
+                                    {
+                                        var name = resource["name"];
+                                        var point = resource["point"];
+                                        var coordinates = point?["coordinates"];
+
+                                        if (name != null && coordinates != null)
+                                        {
+                                            boxlocation.Text = coordinates[0] +"" +coordinates[1];
+                                            /*Console.WriteLine($"Szerokość geograficzna: {coordinates[0]}");
+                                            Console.WriteLine($"Długość geograficzna: {coordinates[1]}");*/
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
